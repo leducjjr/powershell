50242b7c6b1c39bf928414e7a1b8bf7c47826bca
leducjjr <john.v@leduc.com>
2026-03-21 18:54:17 -0500

    Update update_server_and_start_it.ps1

--- palworld/update_server_and_start_it.ps1
+++ palworld/update_server_and_start_it.ps1
@@ -4,7 +4,7 @@ if ([Environment]::UserInteractive) { cls }
 # Set paths for SteamCMD and server installation - EDIT THE PATHS ACCORDINGLY
 $STEAMCMD_PATH = "C:\steamcmd\steamcmd.exe"
 $SERVER_PATH   = "C:\palworldserver"
-$PALWORLD_SERVER_NAME = "xenomenomop"
+$PALWORLD_SERVER_NAME = "yourservernamehere"
 $PALWORLD_PORT        = 8211
 
 # Log file path - named after this script with a timestamp
